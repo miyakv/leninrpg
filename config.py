c4e1b93a3e90eda4bac9283d39d3c58f9276ed85
mcyota <mi2yakv@gmail.com>
2019-01-12 20:48:25 +0400

    draw sprite added

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
 WALL = 1
 LOCATION_EXIT = -1
 PLAYER = 'p'
+import sprites
+
+
+def draw(tile_type, screen, i, j, cell_size):
+    if tile_type == 1:
+        sprites.wall.draw(i, j, cell_size, screen)
+    if tile_type == 'p':
+        sprites.player.draw(i, j, cell_size, screen)
+
+
