6c34a6cb627a5c19368ce92d23ea1b4a6b5ebf22
Kamil <45203116+ulyanovskk@users.noreply.github.com>
2019-01-19 22:33:16 +0400

    class Player

--- lib_sprites.py
+++ lib_sprites.py
@@ -10,19 +10,19 @@ def load_image(name, colorkey=None):
     except pygame.error as message:
         print('Cannot load image:', name)
         raise SystemExit(message)
-
-    image = image.convert_alpha()
     if colorkey is not None:
         if colorkey is -1:
             colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey)
+    image = image.convert_alpha()
     return image
 
 
 all_sprites = pygame.sprite.Group()
 brick_image = load_image('brick.jpg')
-door_image = load_image('door3.png')
+door_image = load_image('door3.png', (255, 255, 255))
 updoor_image = load_image('door.png')
+lenin_image = load_image('lenin.jpg', (255, 255, 255))
 standart_size = 16
 
 
@@ -56,6 +56,17 @@ class UpDoor(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, group, x, y):
+        super().__init__()
+        self.group = group
+        self.image = lenin_image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        all_sprites.add(self)
 
 
 def make_room(sprite, pos, x, y, door_pos_x, door_down=True):
@@ -95,6 +106,7 @@ def make_room(sprite, pos, x, y, door_pos_x, door_down=True):
     all_sprites.add(bricks)
 
 
+player = Player(all_sprites, 30, 26)
 make_room(Brick, (10, 10), 20, 10, 5)
 make_room(Brick, (10, 200), 10, 5, 5, False)
 
@@ -103,6 +115,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player.rect.y += 10
+            if event.key == pygame.K_UP:
+                player.rect.y -= 10
+            if event.key == pygame.K_LEFT:
+                player.rect.x -= 10
+            if event.key == pygame.K_RIGHT:
+                player.rect.x += 10
 
     screen.fill(pygame.Color('white'))
     all_sprites.draw(screen)
