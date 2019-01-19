57de505daaafb39de8baf556b9971693abe9a520
Kamil <45203116+ulyanovskk@users.noreply.github.com>
2019-01-19 19:38:56 +0400

    Add files via upload
    Class Brick

---
+++ lib_sprites.py
@@ -0,0 +1,60 @@
+import pygame
+
+size = width, height = 400, 400
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    try:
+        image = pygame.image.load(name)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    image = image.convert_alpha()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+all_sprites = pygame.sprite.Group()
+brick_image = load_image('brick.jpg')
+standart_size = 16
+
+
+class Brick(pygame.sprite.Sprite):
+    def __init__(self, group, x, y):
+        super().__init__()
+        self.group = group
+        self.image = brick_image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        all_sprites.add(self)
+
+
+def make_room(sprite, x, y):
+    for i in range(x):
+        brick1 = sprite(all_sprites, i*standart_size, 0)
+        brick4 = sprite(all_sprites, i*standart_size, y*standart_size)
+    for j in range(y):
+        brick3 = sprite(all_sprites, x*standart_size, j*standart_size)
+        brick2 = sprite(all_sprites, 0, j*standart_size)
+    brick = sprite(all_sprites, x*standart_size, y*standart_size)
+
+
+make_room(Brick, 20, 10)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(pygame.Color('white'))
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+pygame.quit()
