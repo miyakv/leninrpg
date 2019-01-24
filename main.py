4dbac976bc47928e4ad0748d96cd33011d215560
Kamil <45203116+ulyanovskk@users.noreply.github.com>
2019-01-24 23:06:27 +0400

    Test my door, please

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
 import pygame
-from lib_board import Board
-from lib_location import Location
-from lib_sprites import Player, make_room, Brick
+from lib_sprites import *
 
 pygame.init()
 size = 500, 500
 screen = pygame.display.set_mode(size)
 
+
 class Game:
     def __init__(self):
         self.all_sprites = pygame.sprite.Group()
-        self.player = Player(self.all_sprites, 1, 1)
-        self.locations = {'home': Location(screen, 5, 5, make_room(Brick, (5, 5), 7, 5, 1), (1, 2))}
-        self.current_location = self.locations['home']
-        # all game info here
+        self.player = Player(self.all_sprites, 32, 32)
+        self.bricks = set()
+        for i in range(6):
+            self.bricks.add(Brick(self.all_sprites, 0, i * config.TILE_SIZE))
+        for i in range(8):
+            self.bricks.add(Brick(self.all_sprites, i * config.TILE_SIZE, 0))
+        for i in range(6):
+            self.bricks.add(Brick(self.all_sprites, config.TILE_SIZE * 7, i * config.TILE_SIZE))
+        for i in range(8):
+            if i != 5:
+                self.bricks.add(Brick(self.all_sprites, i * config.TILE_SIZE, config.TILE_SIZE * 5))
+        self.doors = set()
+        self.doors.add(Door(self.all_sprites, 80, 80))
 
-jeu = Game()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                        for door in self.doors:
+                            door.get_event(event)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.player.move('up')
+                    if event.key == pygame.K_a:
+                        self.player.move('left')
+                    if event.key == pygame.K_s:
+                        self.player.move('down')
+                    if event.key == pygame.K_d:
+                        self.player.move('right')
+            screen.fill((202, 255, 255))
+            self.all_sprites.draw(screen)
+            pygame.display.flip()
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                jeu.player.move('up')
-            if event.key == pygame.K_a:
-                jeu.player.move('left')
-            if event.key == pygame.K_s:
-                jeu.player.move('down')
-            if event.key == pygame.K_d:
-                jeu.player.move('right')
-    screen.fill((0, 0, 0))
-    jeu.current_location.render_sprites(jeu.all_sprites) # unfinished!!1!
-    pygame.display.flip()
-pygame.quit()
+        pygame.quit()
+
+
+game = Game()
